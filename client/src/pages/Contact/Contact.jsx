fe23e173741092e32b56bf0fa6b79d065b1ff842
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2024-02-09 10:36:52 -0800

    added styling for contact page,and h2 font sizing

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import css from "./Contact.module.css";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import TestimonialForm from "../../components/TestimonialsForm/TestimonialForm";
 
 const Contact = () => {
   return (
-    <main id="contactPage">
+    <main id="contactPage" className={css.contactPageContainer}>
       <ContactForm />
       <TestimonialForm />
     </main>
