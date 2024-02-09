67cbb0a1ed3f26082526cfc1d20a168e6e763bec
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2024-02-09 11:38:43 -0800

    book page added

--- client/src/pages/Books/Books.jsx
+++ client/src/pages/Books/Books.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import BookFrontPage from "../../assets/imgs/BookFrontPage.png";
 import css from "./books.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Books = () => {
+  const navigate = useNavigate();
+  const handleBookClick = () => {
+    navigate("/freeSense");
+  };
+
   return (
     <main id="books">
       <section className={css.bookContainer}>
-        <img src={BookFrontPage} alt="book-cover" />
-        <h2>FreeSense</h2>
+        <div className={css.eachBookContainer}>
+          <img
+            src={BookFrontPage}
+            alt="book-cover"
+            className={css.bookCover}
+            onClick={handleBookClick}
+          />
+          <h2 onClick={handleBookClick}>FreeSense</h2>
+        </div>
       </section>
     </main>
   );
