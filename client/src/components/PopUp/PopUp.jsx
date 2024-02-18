d9987f14ec1e14ea67edfdcb4a867575a7ecacb6
simo M <simo.mordi@gmail.com>
2024-02-18 01:47:23 -0800

    popUp

--- client/src/components/PopUp/PopUp.jsx
+++ client/src/components/PopUp/PopUp.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import css from './PopUp.module.css'; // This imports the CSS module.
-import frontCover from "../../assets/imgs/BookFrontPage.png"; // This imports the front cover image.
-import backCover from "../../assets/imgs/BookBackCover.png"; // This imports the back cover image.
+import css from './PopUp.module.css'; 
+import frontCoverImage from "../../assets/imgs/BookFrontPage.png"; 
+import backCoverImage from "../../assets/imgs/BookBackCover.png"; 
 
-const BookComponent = ({ isOpen, closePopup }) => {
+const PopUpComponent = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className={css.overlay}>
-      <div className={css.popup}>
-        <div className={css.closeButton} onClick={closePopup}>&times;</div>
-        <div className={css.cardContainer}>
-          <div className={css.cardImage}>
-            <img src={frontCover} alt="Front Cover" className={css.frontCover} />
-            <img src={backCover} alt="Back Cover" className={css.backCover} />
-          </div>
-          <div className={css.cardContent}>
-            <h1 className={css.title}>Stand Pat: FreeSense</h1>
-            <h2 className={css.subtitle}>An Anthology by Pat & MORE</h2>
-            <h3 className={css.author}>Patrick Olochan II</h3>
-            <p className={css.description}>
-              Lorem ipsum dolor sit amet consectetur. Eu elit est interdum morbi semper ipsum tellus in...
-            </p>
-            <button className={css.actionButton}>Freshed out the press</button>
-          </div>
-        </div>
+      <div className={css.close} onClick={onClose}>&times;</div>
+      <div className={css.content}>
+        <img src={frontCoverImage} alt="Front Cover" className={css.coverImage} />
+        <img src={backCoverImage} alt="Back Cover" className={css.coverImage} />
+        <p className={css.descriptionText}>
+          Lorem ipsum dolor sit amet consectetur. Eu elit est interdum morbi semper ipsum tellus in. Egestas enim libero porttitor augue lectus cursus orci. Purus magna nunc scelerisque magnis ultrices et senectus enim diam. Vitae ante pulvinar proin nunc quis tristique purus neque pharetra.
+        </p>
+        <button className={css.button}>Freshed out the press</button>
       </div>
     </div>
   );
 };
 
-export default BookComponent;
+export default PopUpComponent;
